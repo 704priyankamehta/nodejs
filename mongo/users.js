9fc39a98effb93d8e052cc94f023b5f4a8cd4efd
704priyankamehta <priyanka.mehta@kelltontech.com>
2020-08-04 19:19:58 +0530

    commit

--- mongo/users.js
+++ mongo/users.js
@@ -2,7 +2,9 @@ const mongoose  = require("mongoose");
  var Schema=mongoose.Schema;
 
 var user=new Schema({
-    name:String
+    name:String,
+    item:String,
+    status:String
 });
 var userModel=mongoose.model('userModel',user);
 
